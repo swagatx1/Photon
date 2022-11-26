d56ce8b43f421ae5b46968269b19e8b4c71ee789
swagatx1 <swagatrout02@gmail.com>
2022-11-26 16:03:03 +0530

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,41 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://cdn.pixabay.com/photo/2016/07/03/18/36/youtube-1495277_960_720.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/an_webp/E4lb2gkyXr8/mqdefault_6s.webp?du=3000&sqp=CPOOyJkG&rs=AOn4CLD_h6BhDNyYSWI9qDxlenq2JMYe3A';
+export const demoChannelUrl = 'https://www.youtube.com/c/cs50';
+export const demoVideoUrl = 'https://youtu.be/E4lb2gkyXr8';
+export const demoChannelTitle = 'CS50';
+export const demoVideoTitle = 'Structures - CS50 Shorts';
+export const demoProfilePicture = 'https://yt3.ggpht.com/ytc/AMLnZu_8E2fVaNH2D20xn32ph9axohUCgWaLpXO_tXJ14g=s88-c-k-c0x00ffffff-no-rj';
